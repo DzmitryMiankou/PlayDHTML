8b21e9fc4feb94d7a6e0ccd6e01ad6dd3c83681b
DzmitryMiankou <gmiankou@gmail.com>
2022-05-17 22:14:23 +0300

    veryGood

--- script.js
+++ script.js
@@ -2,16 +2,30 @@
 const parent1 = document.getElementById("list2");
 const elem1 = document.getElementById("sound1");
 const elem2 = document.getElementById("sound2");
+const delelem1 = document.getElementById("soundNew1");
 
-/*Функция, создающая клон*/
-const createCopy = (a,b) => {
+/*Клонирование*/
+const createCopy = (a,b,c) => {
 let clone = a.cloneNode(true);
 let list = (b);
-let thisClone = document.createElement(list.appendChild(clone));
+let copy = list.append(clone);
+let copeDom = document.createElement(copy);
+copeDom.append(copy)
+clone.removeAttribute("id");
+clone.setAttribute("id",c);
+clone.ondblclick = () => {
+    clone.remove();
 }
+}
+/*Активация*/
+const getOnclik = (a,b,c) => {
+a.onclick = () =>{
+createCopy(a,b,c)};
+}
+/*Удаление*/
+
+/*Значения*/
+getOnclik(elem1,parent1,"soundNew1");
+getOnclik(elem2, parent1,"soundNew2");
+
 /*ondblclick - реакция на два клика*/
-/*Вызова функции*/
-document.getElementById("sound1").onclick = () =>{
-createCopy(elem1, parent1)};
-document.getElementById("sound2").onclick = () =>{
-createCopy(elem2, parent1)};
