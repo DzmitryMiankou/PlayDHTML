aa0b22dd2866c71a48a083d8408279c23f8afe4e
DzmitryMiankou <gmiankou@gmail.com>
2022-05-17 14:25:02 +0300

    17(goodFunctionCreateCopy)

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 "use strict"
-/*dblclick  - событие двойного клика*/ 
-const getCopy = document.getElementById("sound1").onclick = () => {
- let parent = document.getElementById('sound1');
- let elem = parent.querySelector('.sound11');
- let clone = elem.cloneNode(true);
- parent.appendChild(clone);
-    } 
+let parent = document.getElementById("listT");
+let elem = document.querySelector(".sound12");
+
+/*Функция, создающая клон*/
+let createCopy = (a,b) => {
+let clone = a.cloneNode(true);
+let list = (b);
+let item = document.createElement(list.appendChild(clone));
+}
+/*Вызова функции*/
+document.getElementById("sound2").onclick = () =>{
+createCopy(elem, parent)};
