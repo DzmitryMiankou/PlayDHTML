7713bfaefcfee96f097f7217a182436a6a7d7417
DzmitryMiankou <gmiankou@gmail.com>
2022-05-17 11:25:39 +0300

    17(1)may

--- script.js
+++ script.js
@@ -1,2 +1,8 @@
 "use strict"
-f.after(g);
+/*dblclick  - событие двойного клика*/ 
+const getCopy = document.getElementById("sound1").onclick = () => {
+ let parent = document.getElementById('sound1');
+ let elem = parent.querySelector('.sound11');
+ let clone = elem.cloneNode(true);
+ parent.appendChild(clone);
+    } 
