774c65e7078dde2c618458f6e234b4844ef26ef7
DzmitryMiankou <gmiankou@gmail.com>
2022-05-17 19:05:08 +0300

    17(3)good

--- script.js
+++ script.js
@@ -1,13 +1,17 @@
 "use strict"
-let parent = document.getElementById("listT");
-let elem = document.querySelector(".sound12");
+const parent1 = document.getElementById("list2");
+const elem1 = document.getElementById("sound1");
+const elem2 = document.getElementById("sound2");
 
 /*Функция, создающая клон*/
-let createCopy = (a,b) => {
+const createCopy = (a,b) => {
 let clone = a.cloneNode(true);
 let list = (b);
-let item = document.createElement(list.appendChild(clone));
+let thisClone = document.createElement(list.appendChild(clone));
 }
+/*ondblclick - реакция на два клика*/
 /*Вызова функции*/
+document.getElementById("sound1").onclick = () =>{
+createCopy(elem1, parent1)};
 document.getElementById("sound2").onclick = () =>{
-createCopy(elem, parent)};
+createCopy(elem2, parent1)};
